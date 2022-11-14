09b9518eef6d11db42953dcd6040fbe4d5bacc66
robin <robin@wiillus.com>
2022-11-14 18:19:57 +0900

    added react-hook-form lib

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,6 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import Layout from "../components/layout/Layout";
-import "animate.css";
 
 export default function App({ Component, pageProps }: AppProps) {
 	return (
