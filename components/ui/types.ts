233aba6b515cef030c4c351e3d352d1077237668
robin <robin@wiillus.com>
2022-11-15 18:10:46 +0900

    fixed type file missing issue

--- components/ui/types.ts
+++ components/ui/types.ts
@@ -1,3 +1,10 @@
+export interface RadioOptionProps {
+	label?: string;
+	value: string | number;
+	checked?: boolean;
+}
 
 export type SexType = "female" | "male" | "unselected";
 export type AgeType = "20" | "30" | "40" | "50+";
+
+export type AdPlanType = "basic" | "premium";
