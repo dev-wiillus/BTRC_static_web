a4763f656eba6ee1c9c43e9acdffa65a39a8b31b
robin <robin@wiillus.com>
2022-11-15 18:38:05 +0900

    fixed Hydration failed issue

--- components/layout/Header.tsx
+++ components/layout/Header.tsx
@@ -5,16 +5,9 @@ import Image from "next/image";
 function Header() {
 	return (
 		<header className="absolute flex h-[56px] w-full items-center justify-between px-4 md:px-10">
-			<div className="header__logo">
-				<Link href="/">
-					<Image
-						src="/images/logo/BTRC.png"
-						alt="Logo"
-						width={101}
-						height={30}
-					/>
-				</Link>
-			</div>
+			<Link href="/">
+				<Image src="/images/logo/BTRC.png" alt="Logo" width={101} height={30} />
+			</Link>
 			<Navigation />
 		</header>
 	);
