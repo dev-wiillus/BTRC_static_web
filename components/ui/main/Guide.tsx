a8d57feaec3a5680f35f5a7853abf71dc2a78eb3
robin <robin@wiillus.com>
2022-11-21 12:18:50 +0900

    main/guide ui 수정

--- components/ui/main/Guide.tsx
+++ components/ui/main/Guide.tsx
@@ -23,12 +23,16 @@ export default function Guide() {
 	}, []);
 
 	return (
-		<section className="flex min-h-screen flex-col items-center justify-center py-40 text-center">
-			<div className="reveal fade-bottom my-40 text-[40px] font-bold leading-[44px]">
+		<section className="relative flex min-h-screen flex-col items-center py-40 text-center md:h-[2446px]">
+			<div className="reveal fade-bottom my-40 text-[40px] font-bold leading-[44px] md:text-secondary">
 				<h1>해볼래?</h1>
 				<h1>누구나 할 수 있어.</h1>
 			</div>
 
+			<div
+				className="absolute top-[455px] hidden w-full md:block md:bg-[url('../public/images/main/identity_line_web.png')]"
+				style={{ height: "-webkit-fill-available" }}
+			/>
 			<div className="md:hidden">
 				<div className="reveal fade-bottom my-40 font-bold">
 					<span className="text-[64px]">받고,</span>
@@ -80,11 +84,8 @@ export default function Guide() {
 				</div>
 			</div>
 
-			<div className="reveal fade-bottom my-40 hidden h-[250px] w-full items-center px-[80px] font-bold md:flex">
-				<div className="flex-1">
-					<span className="animate-[highlight-action1_8s_linear_infinite]">
-						받고,
-					</span>
+			<div className="my-36 hidden h-[250px] w-full flex-col gap-y-[200px] px-[250px] font-bold md:flex">
+				<div className="reveal fade-right flex flex-1 gap-x-[120px] self-start">
 					<div className="relative h-[275px] w-[302.6px] rounded-[1rem]">
 						<Image
 							src="/images/main/identity_grab.png"
@@ -92,36 +93,55 @@ export default function Guide() {
 							objectFit="cover"
 							alt="identity-grab"
 						/>
-						{/* <p className="mt-[73px] animate-[highlight-text1_8s_linear_infinite] text-text-gray-primary">
+					</div>
+					<div>
+						<span className="text-[56px]">받고,</span>
+						<p className="mt-[42px] text-[24px] font-semibold text-text-gray-primary">
 							원하는 텀블러에
 							<br />
 							무료커피 받고
-						</p> */}
+						</p>
 					</div>
 				</div>
-				<div className="flex-1">
-					<span className="animate-[highlight-action2_8s_linear_infinite]">
-						즐기고,
-					</span>
-					<p className="mt-[73px] animate-[highlight-text2_8s_linear_infinite] text-text-gray-primary">
-						일주일간
-						<br />
-						즐기고
-					</p>
+				<div className="reveal fade-left flex flex-1 gap-x-[120px] self-end">
+					<div>
+						<span className="text-[56px]">즐기고,</span>
+						<p className="mt-[42px] text-[24px] font-semibold text-text-gray-primary">
+							일주일간
+							<br />
+							즐기고
+						</p>
+					</div>
+					<div className="relative h-[275px] w-[302.6px] rounded-[1rem]">
+						<Image
+							src="/images/main/identity_put.png"
+							layout="fill"
+							objectFit="cover"
+							alt="identity-grab"
+						/>
+					</div>
 				</div>
-				<div className="flex-1">
-					<span className="animate-[highlight-action3_8s_linear_infinite]">
-						반납하고,
-					</span>
-					<p className="mt-[73px] animate-[highlight-text3_8s_linear_infinite] text-text-gray-primary">
-						BTRC 스팟에
-						<br />
-						반납하고
-					</p>
+				<div className="reveal fade-right flex flex-1 gap-x-[120px] self-start">
+					<div className="relative h-[275px] w-[302.6px] rounded-[1rem]">
+						<Image
+							src="/images/main/identity_grab.png"
+							layout="fill"
+							objectFit="cover"
+							alt="identity-grab"
+						/>
+					</div>
+					<div>
+						<span className="text-[56px]">반납하고,</span>
+						<p className="mt-[42px] text-[24px] font-semibold text-text-gray-primary">
+							BTRC 스팟에
+							<br />
+							반납하고
+						</p>
+					</div>
 				</div>
 			</div>
 
-			<div className="reveal fade-bottom my-40 text-[40px] font-bold leading-[45px]">
+			<div className="reveal fade-bottom my-40 text-[40px] font-bold leading-[45px] md:absolute md:bottom-[0px] md:text-white">
 				<h1 className="mb-4 text-[32px] font-medium">그리고</h1>
 				<h1>세척과 살균은</h1>
 				<h1>
--- components/utils/cache.ts
+++ components/utils/cache.ts
@@ -2,22 +2,4 @@ import NodeCache from "node-cache";
 
 const nodeCache = new NodeCache({stdTTL: 60 * 3});
 
-class Cache {
-    cache;
-
-    constructor(){
-        this.cache = nodeCache;
-    }
-
-    getCache(key: string){
-        return this.cache.get(key);
-    }
-
-    setCache(key: string, value: string | number){
-        this.cache.set(key, value);
-    }
-};
-
-// export default function cache(){ return new Cache(); };
-
 export default nodeCache
