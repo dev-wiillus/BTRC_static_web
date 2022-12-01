a5db082a47825c2c152b9a1eb2d17fb0b0437bc8
robin <robin@wiillus.com>
2022-12-01 12:00:25 +0900

    ui 수정(guide image)

--- components/ui/main/Guide.tsx
+++ components/ui/main/Guide.tsx
@@ -4,60 +4,60 @@ import useRevealAnimation from "../../utils/useRevealAnimation";
 export default function Guide() {
 	useRevealAnimation();
 	return (
-		<section className="relative flex min-h-screen flex-col items-center py-40 text-center md:h-[2446px]">
-			<div className="reveal fade-bottom my-40 text-[40px] font-bold leading-[44px] md:text-secondary">
+		<section className="relative flex min-h-screen flex-col items-center py-40 text-center md:h-[2446px] md:py-0">
+			<div className="reveal fade-bottom my-48 text-[40px] font-bold leading-[44px] md:mt-60 md:text-secondary">
 				<h1>해볼래?</h1>
 				<h1>누구나 할 수 있어.</h1>
 			</div>
 
 			<div
-				className="absolute top-[455px] hidden w-full md:block md:bg-[url('../public/images/main/identity_line_web.png')]"
+				className="absolute top-[379px] hidden w-full md:block md:bg-[url('../public/images/main/guide_line_web.png')]"
 				style={{ height: "-webkit-fill-available" }}
 			/>
 			<div className="md:hidden">
-				<div className="reveal fade-bottom my-40 font-bold">
+				<div className="reveal fade-bottom my-48 font-bold">
 					<span className="text-[64px]">받고,</span>
-					<div className="mt-[53px]">
+					<div className="mt-[32px]">
 						<Image
-							src="/images/main/identity_grab.png"
-							width={302.6}
-							height={275}
-							alt="identity-grab"
+							src="/images/main/guide_get.png"
+							width={300}
+							height={224.89}
+							alt="guide-get"
 						/>
 					</div>
-					<p className="mt-[73px] text-[24px] leading-[28.8px] text-text-gray-primary">
+					<p className="mt-[32px] text-[24px] leading-[28.8px] text-text-gray-primary">
 						원하는 텀블러에
 						<br />
 						무료커피 받고
 					</p>
 				</div>
-				<div className="reveal fade-bottom my-40 font-bold">
+				<div className="reveal fade-bottom my-48 font-bold">
 					<span className="text-[64px] font-bold">즐기고,</span>
-					<div className="mt-[53px]">
+					<div className="mt-[32px]">
 						<Image
-							src="/images/main/identity_put.png"
-							width={302.6}
-							height={275}
-							alt="identity-put"
+							src="/images/main/guide_drink.png"
+							width={300}
+							height={224.89}
+							alt="guide-drink"
 						/>
 					</div>
-					<p className="mt-[73px] text-[24px] leading-[28.8px] text-text-gray-primary">
+					<p className="mt-[32px] text-[24px] leading-[28.8px] text-text-gray-primary">
 						일주일간
 						<br />
 						즐기고
 					</p>
 				</div>
-				<div className="reveal fade-bottom my-40 font-bold">
+				<div className="reveal fade-bottom my-48 font-bold">
 					<span className="text-[64px] font-bold">반납하고,</span>
-					<div className="mt-[53px]">
+					<div className="mt-[32px]">
 						<Image
-							src="/images/main/identity_put.png"
-							width={302.6}
-							height={275}
-							alt="identity-put"
+							src="/images/main/guide_return.png"
+							width={300}
+							height={224.89}
+							alt="guide-return"
 						/>
 					</div>
-					<p className="mt-[73px] text-[24px] leading-[28.8px] text-text-gray-primary">
+					<p className="mt-[32px] text-[24px] leading-[28.8px] text-text-gray-primary">
 						BTRC 스팟에
 						<br />
 						반납하고
@@ -65,14 +65,10 @@ export default function Guide() {
 				</div>
 			</div>
 
-			<div className="my-36 hidden h-[250px] w-full flex-col gap-y-[200px] px-[250px] font-bold md:flex">
-				<div className="reveal fade-right flex flex-1 gap-x-[120px] self-start">
-					<div className="relative h-[275px] w-[302.6px] rounded-[1rem]">
-						<Image
-							src="/images/main/identity_grab.png"
-							fill
-							alt="identity-grab"
-						/>
+			<div className="my-24 hidden h-[250px] w-full flex-col gap-y-[279px] px-[250px] font-bold md:flex">
+				<div className="reveal fade-right flex flex-1 gap-x-[52px] self-start">
+					<div className="relative h-[224.89px] w-[300px] rounded-[1rem]">
+						<Image src="/images/main/guide_get.png" fill alt="guide-get" />
 					</div>
 					<div>
 						<span className="text-[56px]">받고,</span>
@@ -83,7 +79,7 @@ export default function Guide() {
 						</p>
 					</div>
 				</div>
-				<div className="reveal fade-left flex flex-1 gap-x-[120px] self-end">
+				<div className="reveal fade-left mr-20 flex flex-1 gap-x-[79px] self-end">
 					<div>
 						<span className="text-[56px]">즐기고,</span>
 						<p className="mt-[42px] text-[24px] font-semibold text-text-gray-primary">
@@ -92,20 +88,16 @@ export default function Guide() {
 							즐기고
 						</p>
 					</div>
-					<div className="relative h-[275px] w-[302.6px] rounded-[1rem]">
-						<Image
-							src="/images/main/identity_put.png"
-							fill
-							alt="identity-grab"
-						/>
+					<div className="relative h-[224.89px] w-[300px] rounded-[1rem]">
+						<Image src="/images/main/guide_drink.png" fill alt="guide-drink" />
 					</div>
 				</div>
-				<div className="reveal fade-right flex flex-1 gap-x-[120px] self-start">
-					<div className="relative h-[275px] w-[302.6px] rounded-[1rem]">
+				<div className="reveal fade-right flex flex-1 gap-x-[115px] self-start">
+					<div className="relative h-[224.89px] w-[300px] rounded-[1rem]">
 						<Image
-							src="/images/main/identity_grab.png"
+							src="/images/main/guide_return.png"
 							fill
-							alt="identity-grab"
+							alt="guide-return"
 						/>
 					</div>
 					<div>
@@ -119,7 +111,7 @@ export default function Guide() {
 				</div>
 			</div>
 
-			<div className="reveal fade-bottom my-40 text-[40px] font-bold leading-[45px] md:absolute md:bottom-[0px] md:text-white">
+			<div className="reveal fade-bottom my-48 text-[40px] font-bold leading-[45px] md:absolute md:bottom-[0px] md:text-white">
 				<h1 className="mb-4 text-[32px] font-medium">그리고</h1>
 				<h1>세척과 살균은</h1>
 				<h1>
